a00ef5d10a26bdc696bf37694eb642d044972770
codinglife2403 <humasaira028@gmail.com>
2022-07-06 08:47:45 +0500

    First Commit

---
+++ CarParkingCalculator1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class CarParkingCalculator1
+    {
+        static void Main(string[] args)
+        {
+            Employee employeeSalary = new Employee();
+            Engineer engineerSalary = new Engineer();
+            Analyst analystSalary = new Analyst();
+            Manager managerSalary = new Manager();
+            Console.WriteLine("Enter your designation:");
+            string desig = Console.ReadLine();
+            Console.WriteLine("Your gross salary=");
+            if (desig == employeeSalary.designation)
+            {
+                employeeSalary.CalculateSalary();
+            }
+            if (desig == engineerSalary.designation)
+            {
+                engineerSalary.CalculateSalary();
+            }
+            if (desig == managerSalary.designation)
+            {
+                managerSalary.CalculateSalary();
+            }
+            if (desig == analystSalary.designation)
+            {
+                analystSalary.CalculateSalary();
+            }
+
+        }
+        class SalarySlip
+        {
+            public string designation;
+            public int fuelCost;
+            public int medicalAllowance;
+            protected static int baseSalary = 1500;
+            public void CalculateSalary()
+            {
+
+            }
+        }
+        class Employee : SalarySlip
+        {
+            public string designation = "Employee";
+            public void CalculateSalary()
+            {
+                int salary = 0; ;
+                salary = baseSalary;
+                Console.WriteLine(salary);
+            }
+        }
+        class Engineer : SalarySlip
+        {
+            public string designation = "Engineer";
+            static int fuelCost = 100;
+            static int medicalAllowance =500;
+            public void CalculateSalary()
+            {
+                int salary = 0; ;
+                salary = baseSalary + fuelCost + medicalAllowance;
+                Console.WriteLine(salary);
+            }
+        }
+        class Analyst : SalarySlip
+        {
+            public string designation = "Analyst";
+            static int fuelCost = 150;
+            static int medicalAllowance = 800;
+            public void CalculateSalary()
+            {
+                int salary=0;
+                salary = baseSalary + fuelCost + medicalAllowance;
+                Console.WriteLine(salary);
+            }
+        }
+                    class Manager : SalarySlip
+                    {
+                        public string designation = "Manager";
+                        public int fuelCost = 250;
+                        public int medicalAllowance = 1000;
+                        public void CalculateSalary()
+                        {
+                            int salary=0;
+                            salary = baseSalary + fuelCost + medicalAllowance;
+                            Console.WriteLine(salary);
+                        }
+
+                    }
+
+                }
+            }
+       
---
+++ SalarySlip1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bike bike = new Bike();
+            Car car = new Car();
+            Bus bus = new Bus();
+           
+            //you want to know the rent for 3 vehicles
+            int n = 0;
+            while (n < 4)
+            {
+                Console.WriteLine("Enter Vehicle Type:");
+                string vehicle = Console.ReadLine();
+                Console.WriteLine("Number of hours");
+                string num = Console.ReadLine();
+                int numOfHrs = Convert.ToInt32(num);
+                if (vehicle == "bike")
+                {
+                    bike.CalculateRent(numOfHrs);
+                }
+                if (vehicle == "car")
+                {
+                    car.CalculateRent(numOfHrs);
+                }
+                if (vehicle == "bus")
+                {
+                    bus.CalculateRent(numOfHrs);
+                }
+                n = n + 1;
+            }
+
+
+        }
+        class CarParkingCalculator
+        {
+            public void CalculateRent(int numberOfHrs)
+            { }
+        }
+        class Car : CarParkingCalculator
+        {
+            public void CalculateRent(int numberOfHrs)
+            {
+                int threeHrRent = 4;
+                double oneHrRent = 1;
+                int twentyFourHrRent = 20;
+                Console.WriteLine("Rent for" + " " + "Car" + " " + "in dollar=");
+                if (numberOfHrs == 3)
+                {
+                    Console.WriteLine(threeHrRent);
+                }
+                if (numberOfHrs > 3 & numberOfHrs < 24)
+                {
+                    Console.WriteLine(threeHrRent + (numberOfHrs - 3) * oneHrRent);
+                }
+                if (numberOfHrs == 24)
+                {
+                    Console.WriteLine(twentyFourHrRent);
+                }
+            }
+        }
+        class Bike : CarParkingCalculator
+        {
+            public void CalculateRent(int numberOfHrs)
+            {
+                int threeHrRent = 2;
+                double oneHrRent = 0.5;
+                int twentyFourHrRent = 10;
+                Console.WriteLine("Rent for" + " " + "bike" + " " + "in dollar=");
+                if (numberOfHrs == 3)
+                {
+                    Console.WriteLine(threeHrRent);
+                }
+                if (numberOfHrs > 3 & numberOfHrs < 24)
+                {
+                    Console.WriteLine(threeHrRent + (numberOfHrs - 3) * oneHrRent);
+                }
+                if (numberOfHrs == 24)
+                {
+                    Console.WriteLine(twentyFourHrRent);
+                }
+            }
+        }
+                class Bus : CarParkingCalculator
+                {
+                    public void CalculateRent(int numberOfHrs)
+                    {
+                        int threeHrRent = 6;
+                        double oneHrRent = 1.5;
+                        int twentyFourHrRent = 30;
+                        Console.WriteLine("Rent for" + " " + "bus" + " " + "in dollar=");
+                        if (numberOfHrs == 3)
+                        {
+                            Console.WriteLine(threeHrRent);
+                        }
+                        if (numberOfHrs > 3 & numberOfHrs < 24)
+                        {
+                            Console.WriteLine(threeHrRent + (numberOfHrs - 3) * oneHrRent);
+                        }
+                        if (numberOfHrs == 24)
+                        {
+                            Console.WriteLine(twentyFourHrRent);
+                        }
+                    }
+
+                }
+
+            }
+        }
+ 
